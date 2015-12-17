9cc28787b749c655eb096e6f5dc5ec29c6382a7a
Elijah Kim <kimelijah90@gmail.com>
2015-12-17 11:22:34 -0500

    Have a planet! kind of

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 class App
-  attr_reader :input
+  attr_reader :input, :planet
 
   def initialize(input)
-    @input = input
+    set_up_world(input)
   end
 
   def run
@@ -10,5 +10,13 @@ class App
 1 3 N
 5 1 E
      eos
+  end
+
+  private
+
+  def set_up_world(input)
+    input_map = input.split("\n")
+
+    @planet = input_map.shift
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,6 +3,11 @@ require "spec_helper"
 describe App do
   let(:input) { File.read("spec/support/input.txt") }
   let(:subject) { described_class.new(input) }
+
+  it "has the proper attributes" do
+    expect(subject.planet).to eq "5 5"
+  end
+
   it "returns the proper coordinates" do
     expect(subject.run).to eq(
       <<-eos
