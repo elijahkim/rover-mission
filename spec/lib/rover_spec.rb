12e7bb257e43d6e521b28b4332d01efa9a2910f3
Elijah Kim <kimelijah90@gmail.com>
2015-12-17 12:42:15 -0500

    Give rover ability to rotate

--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -6,6 +6,17 @@ describe Rover do
   describe "#move!(instruction)" do
     context "if the instruction is L" do
       it "rotates the rover 90* counter clock wise" do
+        subject.move!("L")
+
+        expect(subject.direction).to eq "W"
+      end
+    end
+
+    context "if the instruction is R" do
+      it "rotates the rover 90* clock wise" do
+        subject.move!("R")
+
+        expect(subject.direction).to eq "E"
       end
     end
   end
